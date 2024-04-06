a1d06a0090ea6d2c5e1904254e3ebcc93de1aab9
Stoian02 <81009844+Stoian02@users.noreply.github.com>
2024-04-06 19:35:55 +0300

    Finished the ListScreen

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,29 +3,32 @@ import { FlatList, StyleSheet, Text, View } from 'react-native';
 
 const ListScreen = () => {
     const friends = [
-        { name: 'Friend #1' },
-        { name: 'Friend #2' },
-        { name: 'Friend #3' },
-        { name: 'Friend #4' },
-        { name: 'Friend #5' },
-        { name: 'Friend #6' },
-        { name: 'Friend #7' },
-        { name: 'Friend #8' },
-        { name: 'Friend #9' },
+        { name: 'Friend #1', age: 28 },
+        { name: 'Friend #2', age: 29 },
+        { name: 'Friend #3', age: 30 },
+        { name: 'Friend #4', age: 31 },
+        { name: 'Friend #5', age: 42 },
+        { name: 'Friend #6', age: 22 },
+        { name: 'Friend #7', age: 12 },
+        { name: 'Friend #8', age: 62 },
+        { name: 'Friend #9', age: 35 },
     ];
 
     return (
         <FlatList
+            showsVerticalScrollIndicator={false}
             keyExtractor={(friend) => friend.name}
             data={friends}
             renderItem={({ item }) => {
-                return <Text>{item.name}</Text>;
-            }} />
+                return <Text style={styles.textStyle}>{item.name} - Age {item.age}</Text>;
+            }}
+        />
     );
 };
 
 const styles = StyleSheet.create({
-
+    textStyle: {
+    }
 });
 
 export default ListScreen;
