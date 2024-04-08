f3859a265283e1484234c77826fe62a8af785b1e
Stoian02 <81009844+Stoian02@users.noreply.github.com>
2024-04-08 11:14:26 +0300

    Added file for ImageScreen

---
+++ src/screens/ImageScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const ImageScreen = () => {
+
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default ImageScreen;
