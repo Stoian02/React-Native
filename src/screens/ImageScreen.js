4258fbac034a94e85ee2c0f1f3eafd2c381d3df9
Stoian02 <81009844+Stoian02@users.noreply.github.com>
2024-04-08 11:34:43 +0300

    Added reusable component for ImageScreen

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ImageDetail from '../components/ImageDetail';
 
 const ImageScreen = () => {
+    return (
+        <View>
+            <ImageDetail title="Forest" />
+            <ImageDetail title="Beach" />
+            <ImageDetail title="Mountain" />
+        </View>
+    );
 
 };
 
 const styles = StyleSheet.create({
-
 });
 
 export default ImageScreen;
