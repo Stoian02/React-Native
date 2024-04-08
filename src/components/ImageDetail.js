851a0024bd283e1fd6ab181670ade015c9560c51
Stoian02 <81009844+Stoian02@users.noreply.github.com>
2024-04-08 12:54:16 +0300

    Finished the reusable component(ex. 39)

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 const ImageDetail = (props) => {
-    return <Text>{props.title}</Text>
+    return <View>
+        <Image source={props.imageSource} />
+        <Text>{props.title}</Text>
+        <Text>Image Score - {props.score}</Text>
+    </View>
 };
 
 const styles = StyleSheet.create({
