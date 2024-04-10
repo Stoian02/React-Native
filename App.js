ac83e014ff81998bd21905028fe4b6b74071452c
Stoian02 <81009844+Stoian02@users.noreply.github.com>
2024-04-10 15:40:09 +0300

    Added random generating colors array

--- App.js
+++ App.js
@@ -8,20 +8,20 @@ import ImageScreen from "./src/screens/ImageScreen";
 import ListScreen from "./src/screens/ListScreen";
 
 const navigator = createStackNavigator(
-    {
-        Home: HomeScreen,
-        Components: ComponentsScreen,
-        List: ListScreen,
-        Image: ImageScreen,
-        Counter: CounterScreen,
-        Color: ColorScreen
+  {
+    Home: HomeScreen,
+    Components: ComponentsScreen,
+    List: ListScreen,
+    Image: ImageScreen,
+    Counter: CounterScreen,
+    Color: ColorScreen,
+  },
+  {
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+      title: "App",
     },
-    {
-        initialRouteName: "Home",
-        defaultNavigationOptions: {
-            title: "App",
-        },
-    }
+  }
 );
 
 export default createAppContainer(navigator);
