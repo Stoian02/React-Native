5ccb6ec56f60aba05e0afac287f24c467549e127
Stoian02 <81009844+Stoian02@users.noreply.github.com>
2024-04-12 22:23:27 +0300

    Added component for SquareSqreen and will use state to update it

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,7 +1,16 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import ColorCounter from "../components/ColorCounter";
 
-const SquareScreen = () => {};
+const SquareScreen = () => {
+    return (
+        <View>
+            <ColorCounter color="Red" />
+            <ColorCounter color="Blue" />
+            <ColorCounter color="Green" />
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({});
 
