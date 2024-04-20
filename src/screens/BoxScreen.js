689b4292aa48ff7e0d8773ca6ef41a27551f95a4
Stoian02 <81009844+Stoian02@users.noreply.github.com>
2024-04-20 17:53:35 +0300

    Finished ex. 79

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,20 +4,32 @@ import { StyleSheet, Text, View } from "react-native";
 const BoxScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.textStyle}>Box screen</Text>
+      <Text style={styles.textOneStyle}>Box screen 1</Text>
+      <Text style={styles.textTwoStyle}>Box screen 2</Text>
+      <Text style={styles.textThreeStyle}>Box screen 3</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    height: 200,
     borderWidth: 3,
     borderColor: "black",
   },
-  textStyle: {
+  textOneStyle: {
     borderWidth: 2,
     borderColor: "red",
-    margin: 20,
+  },
+  textTwoStyle: {
+    borderWidth: 2,
+    borderColor: "red",
+    fontSize: 18,
+    ...StyleSheet.absoluteFillObject,
+  },
+  textThreeStyle: {
+    borderWidth: 2,
+    borderColor: "red",
   },
 });
 
